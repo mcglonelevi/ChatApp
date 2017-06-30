7b94dc8373fdf5a0a17f8f7e905f51e85ee63ebb
Levi McGlone <mcglonelevi@gmail.com>
2017-06-30 18:41:50 -0400

    front-end done

--- chat-app/start.js
+++ chat-app/start.js
@@ -1,8 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('message', function(msg) {
+    console.log(msg.u + ' sent \"' + msg.m + '\"');
+    io.emit('propMessage', msg);
+  });
 });
 
 http.listen(8080, function(){
